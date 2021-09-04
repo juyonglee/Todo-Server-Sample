eed7f5112edd35e5aeb29471a9c1568d5ef58792
juyonglee <juyonglee0208@gmail.com>
2021-09-05 02:43:24 +0900

    Controller Test Code 추가

---
+++ src/test/java/com/gmail/juyonglee0208/controller/TodoControllerTest.java
@@ -0,0 +1,55 @@
+package com.gmail.juyonglee0208.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gmail.juyonglee0208.model.TodoEntity;
+import com.gmail.juyonglee0208.model.TodoRequest;
+import com.gmail.juyonglee0208.service.TodoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TodoController.class)
+class TodoControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    TodoService todoService;
+
+    private TodoEntity expected;
+
+    @BeforeEach
+    void setup() {
+        this.expected = new TodoEntity();
+        expected.setId(123L);
+        expected.setTitle("Test Title");
+        expected.setOrder(0L);
+        expected.setCompleted(false);
+    }
+
+    @Test
+    void create() throws Exception {
+        when(this.todoService.add(any(TodoRequest.class))).then((i) -> {
+            TodoRequest request = i.getArgument(0, TodoRequest.class);
+            return new TodoEntity(this.expected.getId(), request.getTitle(), this.expected.getOrder(), this.expected.getCompleted());
+        });
+        TodoRequest request = new TodoRequest();
+        request.setTitle("Any Title");
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(request);
+
+        this.mvc.perform(post("/").contentType(MediaType.APPLICATION_JSON).content(content)).andExpect(status().isOk()).andExpect(jsonPath("$.title").value("Any Title"));
+    }
+     
+}
